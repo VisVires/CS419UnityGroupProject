33151ae5bbc1dbe518d1fedc8684403e84e2b96d
halleri <halleri@onid.oregonstate.edu>
2016-11-02 12:57:06 -0700

    Add project folder

---
+++ Project/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollower : MonoBehaviour {
+
+	public Transform[] path;
+	public float speed = 5.0f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+
+
+	void Start() {
+		
+	}
+
+	void Update() {
+
+		float dist = Vector3.Distance(path[currentPoint].position, transform.position);
+		transform.position = Vector3.MoveTowards (transform.position,path[currentPoint].position, Time.deltaTime*speed );
+
+		if (dist <= reachDist) {
+			currentPoint++;
+		}		
+	}
+		
+}
