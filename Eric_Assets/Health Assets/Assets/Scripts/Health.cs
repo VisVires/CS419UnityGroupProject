37a316188ac6dc8eab33db368f3e73272078326c
halleri <halleri@onid.oregonstate.edu>
2016-11-14 10:41:33 -0800

    add health

---
+++ Eric_Assets/Health Assets/Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField]
+	private Stat health;
+
+	public void RemoveHealth(float amount) {
+
+		health.CurrentValue -= amount;
+
+		if (health.CurrentValue <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
