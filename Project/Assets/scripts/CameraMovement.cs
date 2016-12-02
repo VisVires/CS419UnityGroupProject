b0871756bce4a162787954005b6b418025114414
Peters2329 <petermi3@oregonstate.edu>
2016-12-02 13:37:40 -0500

    zoom added to camera, mousewheel

--- Project/Assets/scripts/CameraMovement.cs
+++ Project/Assets/scripts/CameraMovement.cs
@@ -8,10 +8,33 @@ public class CameraMovement : MonoBehaviour
 		private float cameraSpeed = 0;
 
 
+		//zoom script variables
+		public float zoomSpeed = 1;
+		public float targetOrtho;
+		public float smoothSpeed = 2.0f;
+		public float minOrtho = 1.0f;
+		public float maxOrtho = 20.0f;
+		
+		
+		// Use this for initialization
+		void Start () {
+			targetOrtho = Camera.main.orthographicSize;
+		}
+		
 		// Update is called once per frame
 		private void Update () 
 		{
 			GetInput();
+			
+			float scroll = Input.GetAxis ("Mouse ScrollWheel");
+			if (scroll != 0.0f) {
+				targetOrtho -= scroll * zoomSpeed;
+				targetOrtho = Mathf.Clamp (targetOrtho, minOrtho, maxOrtho);
+         }
+         
+			Camera.main.orthographicSize = Mathf.MoveTowards (Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+			
+			
 		}
 
 
